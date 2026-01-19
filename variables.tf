51318b68fb9dcd5542411db1f800a1685201b5fc
VikJ13 <jhavikrant7@gmail.com>
2026-01-19 22:38:31 +0530

    inital dry commit-1038

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  description = "Azure region where resources will be created"
+  type        = string
+  default     = "East US"
+}
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "rg-vj-testphase1"
+}
