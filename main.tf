da0463d465d00da9a2521acdb92e706047a3a3a1
VikJ13 <jhavikrant7@gmail.com>
2026-01-19 23:20:12 +0530

    this is maybe last commit

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "azurerm_subnet_network_security_group_association" "web_nsg_assoc" {
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet-scenario-one"
+  name                = "vnet-scenario-onetwo"
   address_space       = ["10.1.0.0/16"]
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
