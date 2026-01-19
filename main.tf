15d25dd9e9ffc8cb5a52d17cc4dec8092215dabb
VikJ13 <jhavikrant7@gmail.com>
2026-01-19 23:16:57 +0530

    i have added some more config

--- main.tf
+++ main.tf
@@ -73,3 +73,10 @@ resource "azurerm_subnet_network_security_group_association" "web_nsg_assoc" {
   subnet_id                 = azurerm_subnet.web_subnet.id
   network_security_group_id = azurerm_network_security_group.web_nsg.id
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet-scenario-one"
+  address_space       = ["10.1.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
